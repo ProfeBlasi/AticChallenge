cde97967c69e350b00d21af321fc081ed0263f07
Ezequiel Blasi <profeblasi@outlook.com>
2021-12-28 21:50:23 -0300

    Funciona swagger y scaffold de la db

--- Atic.API/Atic.API/Startup.cs
+++ Atic.API/Atic.API/Startup.cs
@@ -1,7 +1,9 @@
 using Atic.Bootstrap;
 using Atic.Bootstrap.Providers;
+using Atic.Persistance.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,8 @@ namespace Atic.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AticPruebaCandidatosContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddControllers();
             services.ConfigureServices(Configuration);
         }
